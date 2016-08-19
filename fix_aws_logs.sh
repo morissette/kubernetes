dacde7e58f2d819851a883a84b33dc27a5caae74
Matthew Harris <admin@mattharris.org>
2016-08-19 16:54:52 -0500

    Create fix_aws_logs.sh

---
+++ fix_aws_logs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /tmp
+wget https://raw.githubusercontent.com/morissette/kubernetes/master/fluentd-es.yaml
+cp -v /tmp/fluentd-es.yaml /etc/kubernetes/manifests/fluentd-es.yaml 
+cp -v /tmp/fluentd-es.yaml /srv/salt/fluentd-es/fluentd-es.yaml 
+cp -v /tmp/fluentd-es.yaml /usr/share/google/kubernetes/saltbase/salt/fluentd-es/fluentd-es.yaml
+rm -vf /tmp/fluentd-es.yaml
